6832f70bd7a5bc700d9146d4cb9b8e9cb3587fc2
Alastair Smith <github@alastairsmith.me.uk>
2012-10-14 22:04:46 +0100

    Initial faff with SignalR and Knockout.
    The Knockout bindings are working ok, but I haven't got SignalR hooked
    up properly.  Somehow, $.connection is undefined.

---
+++ src/MirrorPad.Web/Hubs/PadHub.cs
@@ -0,0 +1,13 @@
+﻿using SignalR.Hubs;
+
+namespace MirrorPad.Web.Hubs
+{
+    [HubName("pad")]
+    public class PadHub : Hub
+    {
+        public string Send(string text)
+        {
+            return text;
+        }
+    }
+}
